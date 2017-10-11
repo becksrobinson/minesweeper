ff3f969872f5c3510c10c421186f1395930b7d05
Becks <apple@MacBook-Pro-de-apple-2.local>
2017-10-11 16:25:50 +0100

    initis

---
+++ src/minesweeper.js
@@ -0,0 +1,14 @@
+console.log('This is what a empty board would look like:');
+const blankLine = "  |   |  ";
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
+
+
+
